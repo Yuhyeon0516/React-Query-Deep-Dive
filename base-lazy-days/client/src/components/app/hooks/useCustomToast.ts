17642e879ad9b29b6fb0df1b8ac2377b9a1a6a19
Yuhyeon0516 <rladbgus0516@naver.com>
2023-11-23 12:06:30 +0900

    Created useTreatments custom hook

--- base-lazy-days/client/src/components/app/hooks/useCustomToast.ts
+++ base-lazy-days/client/src/components/app/hooks/useCustomToast.ts
@@ -1,23 +1,17 @@
-import {
-  CloseAllToastsOptions,
-  ToastId,
-  useToast,
-  UseToastOptions,
-} from '@chakra-ui/react';
+import { ToastId, useToast, UseToastOptions } from "@chakra-ui/react";
 
 interface UseCustomToast {
-  (options?: UseToastOptions | undefined): string | number | undefined;
-  close: (id: ToastId) => void;
-  closeAll: (options?: CloseAllToastsOptions | undefined) => void;
-  update(id: ToastId, options): void;
-  isActive: (id: ToastId) => boolean | undefined;
+    (options?: UseToastOptions | undefined): string | number | undefined;
+    close: (id: ToastId) => void;
+    update(id: ToastId, options): void;
+    isActive: (id: ToastId) => boolean | undefined;
 }
 
 // a wrapper around Chakra UI's useToast that has my default options applied
 export function useCustomToast(): UseCustomToast {
-  return useToast({
-    isClosable: true,
-    variant: 'subtle',
-    position: 'bottom',
-  });
+    return useToast({
+        isClosable: true,
+        variant: "subtle",
+        position: "bottom",
+    });
 }
