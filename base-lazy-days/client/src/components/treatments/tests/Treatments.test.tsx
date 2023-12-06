5872d43557e8af02bb98c3ecc36a798f99a30999
Yuhyeon0516 <rladbgus0516@naver.com>
2023-12-06 14:09:22 +0900

    Created Treatments component screen test

--- base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ base-lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -3,6 +3,12 @@ import { render, screen } from "@testing-library/react";
 import { Treatments } from "../Treatments";
 import { renderWithQueryClient } from "test-utils";
 
-test("renders response from query", () => {
+test("renders response from query", async () => {
     renderWithQueryClient(<Treatments />);
+
+    const treatmentTitles = await screen.findAllByRole("heading", {
+        name: /Massage|Facial|Scrub/i,
+    });
+
+    expect(treatmentTitles).toHaveLength(3);
 });
