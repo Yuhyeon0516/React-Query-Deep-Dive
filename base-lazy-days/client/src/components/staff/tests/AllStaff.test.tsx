bfbb21c93b633c725c56817658bf3fcb3e38a48f
Yuhyeon0516 <rladbgus0516@naver.com>
2023-12-06 14:14:40 +0900

    Created AllStaff component screen test

--- base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
+++ base-lazy-days/client/src/components/staff/tests/AllStaff.test.tsx
@@ -1,20 +1,26 @@
-import { screen } from '@testing-library/react';
+import { screen } from "@testing-library/react";
 
-// import { rest } from 'msw';
-// import { defaultQueryClientOptions } from '../../../react-query/queryClient';
-// import { server } from '../../../mocks/server';
-// import { renderWithClient } from '../../../test-utils';
-import { AllStaff } from '../AllStaff';
+import { rest } from "msw";
+import { server } from "../../../mocks/server";
+import { AllStaff } from "../AllStaff";
+import { renderWithQueryClient } from "test-utils";
 
-test('renders response from query', () => {
-  // write test here
+test("renders response from query", async () => {
+    // write test here
+    renderWithQueryClient(<AllStaff />);
+
+    const staffNames = await screen.findAllByRole("heading", {
+        name: /sandra|divya|mateo|michael/i,
+    });
+
+    expect(staffNames).toHaveLength(4);
 });
 
-test('handles query error', async () => {
-  // (re)set handler to return a 500 error for staff
-  // server.resetHandlers(
-  //   rest.get('http://localhost:3030/staff', (req, res, ctx) => {
-  //     return res(ctx.status(500));
-  //   }),
-  // );
+test("handles query error", async () => {
+    // (re)set handler to return a 500 error for staff
+    // server.resetHandlers(
+    //   rest.get('http://localhost:3030/staff', (req, res, ctx) => {
+    //     return res(ctx.status(500));
+    //   }),
+    // );
 });
